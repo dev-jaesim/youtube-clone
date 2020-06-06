b3476b87f13e81e5e85030ce0c87770671ca23ba
dev-jaesim <dev.jaesim@gmail.com>
2020-06-06 18:54:14 +1000

    deploy test10

--- server/server.js
+++ server/server.js
@@ -24,9 +24,9 @@ mongoose
   .then(() => console.log("MongoDB is successfully connected"))
   .catch((err) => console.log(err));
 
-app.get("/", function (req, res) {
-  res.send("Server is currently running test456");
-});
+// app.get("/", function (req, res) {
+//   res.send("Server is currently running test456");
+// });
 
 app.use("/uploads", express.static("uploads"));
 
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
