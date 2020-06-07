7bf4b98cbadadeb3895d4c8289d341de1e609d46
dev-jaesim <dev.jaesim@gmail.com>
2020-06-07 12:14:00 +1000

    heroku deploy test

--- server/server.js
+++ server/server.js
@@ -38,6 +38,14 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// for heroku local
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+});
+//
+
 app.use("/api/users", require("./routes/userRoute"));
 app.use("/api/videos", require("./routes/videoRoute"));
 app.use("/api/subscriptions", require("./routes/subscriptionRoute"));
