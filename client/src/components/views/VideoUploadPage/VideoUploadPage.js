2ab8a3ee9cc0091a4e55ada675f481083e5e7a61
dev-jaesim <dev.jaesim@gmail.com>
2022-01-11 23:01:09 +1100

    Added the video upload and thumnail creation

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,70 +1,148 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 import Dropzone from 'react-dropzone';
-import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import { Typography, Button, Form, message, Input } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
 const { Title } = Typography;
 const { TextArea } = Input;
+
+const PrivateOptions = [
+    { value: 0, label: 'Private' },
+    { value: 1, label: 'Public' }
+];
+
+const CategoryOptions = [
+    { value: 0, label: 'Film & Animation' },
+    { value: 1, label: 'Racing' },
+    { value: 2, label: 'Music' },
+    { value: 3, label: 'Animals' }
+];
 
 function VideoUploadPage() {
     const userState = useSelector(state => state.user);
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if(userState.userData === undefined || !userState.userData.isAuth) {
-            navigate('/login');        
-        }
-    });
+    const [VideoTitle, setVideoTitle] = useState('');
+    const [Description, setDescription] = useState('');
+    const [Private, setPrivate] = useState(0);
+    const [Category, setCategory] = useState('Film & Animation');
+    const [FilePath, setFilePath] = useState('');
+    const [Duration, setDuration] = useState('');
+    const [ThumbnailPath, setThumbnailPath] = useState('');
+
+    // useEffect(() => {
+    //     if(userState.userData === undefined || !userState.userData.isAuth) {
+    //         navigate('/login');        
+    //     }
+    // });
+
+    const onTitleChange = e => {
+        setVideoTitle(e.currentTarget.value);
+    };
+
+    const onDescriptionChange = e => {
+        setDescription(e.currentTarget.value);
+    };
+
+    const onPrivateChange = e => {
+        setPrivate(e.currentTarget.value);
+    };
+
+    const onCategoryChange = e => {
+        setCategory(e.currentTarget.value);
+    }
+
+    const onDrop = (files) => {
+        const formData = new FormData();
+        const config = {
+            header: { 'content-type': 'multipart/form-data' }
+        };
+        formData.append("file", files[0]);
+
+        axios.post('/api/video/upload', formData, config)
+            .then(response => {
+                if (response.data.success) {
+                    const variable = {
+                        filePath: response.data.filePath,
+                        fileName: response.data.fileName
+                    };
+
+                    setFilePath(response.data.filePath);
+
+                    axios.post('/api/video/thumbnail', variable)
+                        .then(response => {
+                            if (response.data.success) {
+                                setDuration(response.data.fileDuration);
+                                setThumbnailPath(response.data.thumbsFilePath);
+                            } else {
+                                alert('Failed to make thumbnails');
+                            }
+                        });
+                } else {
+                    alert('failed to save the video in server');
+                }
+            });
+    };
 
     return (
-        <div>
-            <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
                 <Title level={2}>Upload Video</Title>
             </div>
 
-            <Form wrapperCol={{ span: 16 }} style={{ margin: 'auto', width: '50%' }}>
+            <Form wrapperCol={{ span: 16 }} style={{ margin: 'auto'}}>
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     {/* Drop zone */}
                     <Dropzone
-                        onDrop
-                        multiple
-                        maxSize
+                        onDrop={onDrop}
+                        multiple={false}
+                        maxSize={10000000000}
                     >
                         {({ getRootProps, getInputProps }) => (
                             <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
                                 {...getRootProps()}
                             >
                                 <input {...getInputProps()} />
-                                {/* <Icon type="plus" style={{ fontSize: '3rem' }} /> */}
-                                +
+                                <PlusOutlined />
                             </div>
                         )}
                     </Dropzone>
 
                     {/* Thumbnail */}
-                    <div>
-                        <img />
-                    </div>
+                    {ThumbnailPath && 
+                        <div>
+                            <img src={`http://localhost:5000/${ThumbnailPath}`} alt='thumbnail' />
+                        </div>
+                    }
                 </div>
-                <br />
-                <br />
+                <br /><br />
                 <label>Title</label>
-                <Input />
-                <br />
-                <br />
+                <Input 
+                    onChange={onTitleChange}
+                    value={VideoTitle}
+                />
+                <br /><br />
                 <label>Description</label>
-                <TextArea />
-                <br />
-                <br />
-                <select>
-                    <option></option>
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value={Description}
+                />
+                <br /><br />
+                <select onChange={onPrivateChange} value={Private}>
+                    {PrivateOptions.map((item, index) => (
+                        <option key={index} value={item.value}> {item.label} </option>
+                    ))}
                 </select>
-
-                <select>
-                    <option></option>
+                <br /><br />
+                <select onChange={onCategoryChange} value={Category}>
+                    {CategoryOptions.map((item, index) => (
+                        <option key={index} value={item.value}> {item.label} </option>
+                    ))}
                 </select>
-                <br />
-                <br />
+                <br /><br />
                 <Button type='primary' size='large'>
                     Submit
                 </Button>
--- server/index.js
+++ server/index.js
@@ -3,11 +3,16 @@ const app = express();
 const config = require('./config/key');
 
 // MongoDB connection
-const mongoose = require('mongoose');
-// useNewUrlParser is required to remove 'Deprecated' warnings
-mongoose.connect(config.mongoURI, {useNewUrlParser: true})
-    .then(() => console.log('DB successfully connected'))
-    .catch((err) => console.error(err));
+// const mongoose = require('mongoose');
+// // useNewUrlParser is required to remove 'Deprecated' warnings
+// mongoose.connect(config.mongoURI, {useNewUrlParser: true})
+//     .then(() => console.log('DB successfully connected'))
+//     .catch((err) => console.error(err));
+
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -19,6 +24,23 @@ app.use(cookieParser());
 app.get('/', (req, res) => res.send('Hello World!!'));
 
 app.use('/api/user', require('./routes/user'));
+app.use('/api/video', require('./routes/video'));
+
+//use this to show the image you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use('/uploads', express.static('uploads'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
